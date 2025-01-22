d4aa6b5ba6b0db841e381818f52b2fa5901bfae1
vlkhvnn <tanir.alikh@gmail.com>
2025-01-23 03:14:53 +0500

    feat: deleting logo from footer

--- src/scenes/footer/index.tsx
+++ src/scenes/footer/index.tsx
@@ -1,11 +1,8 @@
-import Logo from "@/assets/Logo.png";
-
 const Footer = () => {
   return (
     <footer className="bg-primary-100 py-16">
       <div className="justify-content mx-auto w-5/6 gap-16 md:flex">
         <div className="mt-16 basis-1/2 md:mt-0">
-          <img alt="logo" src={Logo} />
           <p className="my-5">
             Lorem vitae ut augue auctor faucibus eget eget ut libero. Elementum
             purus et arcu massa dictum condimentum. Augue scelerisque iaculis
